4dc912e1634c04f8af7e0d0c1fd8b7f3feb972ad
Ryan <81720741+ryantheawkward@users.noreply.github.com>
2021-05-12 15:16:30 -0400

    Add files via upload

---
+++ src/MainContent.svelte
@@ -0,0 +1,48 @@
+<!-- SVELTE STARTS HERE -->
+
+<script>
+</script>
+
+<!-- ELEMENT TAG STARTS HERE -->
+<svelte:options tag="main-content" />
+
+<!-- CSS STARTS HERE -->
+
+<style>
+    #greet{
+        text-align: center;
+    }
+.mc-con{
+    border: 3px solid rgba(29, 217, 17, 0.76);
+}
+</style>
+
+<!-- HTML STARTS HERE -->
+<div id="main-con-con">
+    <div id="greet">
+        <h1>Welcome To</h1>
+        <h2>NitLyn</h2>
+        <fieldset id="content" class="mc-con">
+        <!-- Main Content Goes Here Sep with <hr> <div> -->
+            <div class="qb-mat">
+                <h1>Upload.</h1>
+                <h3>Compress.</h3>
+                <h4>SHARE!</h4>
+                </div>
+            <p>NitLyn, Because Free Is Better!</p>
+            <hr class="mainc-divider">
+            <div id="mainc-about" class="main-abt">
+               <!-- <p>Using NitLyn's File Compressor your files</p> -->
+                <p>NOTE: this version of NitLyn is a very <i>skinned down version</i> of NitLyn's FC (File Compressor)</p>
+                <p>Meaning that the compressor may break or not work to it's full potential</p>
+               <!-- <h1>Something goes here</h1> -->
+            </div>
+            <hr>
+            <div id="mainc-about2" class="main-abt">
+                <p>You always hear company's saying that there trying to change the world, and for the most part they don't do anything. Here at NitLyn we're not going to say that we're trying to change the world or make it a better place. We're just here to change your file sizes.</p>
+
+            </div>
+        </fieldset>
+    </div>
+   <betw1-08></betw1-08>
+</div>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './App.svelte';
+import './content/NavBar.svelte';
+import './content/Downloads.svelte';
+import './content/Footer.svelte';
+import './content/Ourteam.svelte';
+import './content/About.svelte';
+import './MainContent.svelte';
+import './content/Faq.svelte';
+import './content/ads/ad1.svelte';
+
+
